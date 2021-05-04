23689a079c2a14f1071fb54bccdd877244755ebf
vustsinovich <vustsinovich@demandbase.com>
2021-05-04 16:39:52 +0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: ['https://localhost:3000'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
